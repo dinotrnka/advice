6f4ece626f4b8c44c12a9199980912038534a858
dinotrnka <dinotrnka@gmail.com>
2018-07-27 21:40:47 +0200

    Firebase initial setup

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>
-      Open up App.js to start working on your app!
-    </Text>
-  </View>
-);
+import { auth } from './src/config/firebase';
+
+console.disableYellowBox = true;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await auth.signInWithEmailAndPassword('dinaga@gmail.com', 'password');
+      console.log('RES', res);
+    } catch (e) {
+      console.log('ERROR', e.message);
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Open up App.js to start working on your app!
+        </Text>
+      </View>
+    );
+  }
+};
 
 export default App;
 
---
+++ src/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = 'AIzaSyCkST8jEMPoiNLzqusRAbfii3HlNUdDy6w';
+export const FIREBASE_AUTH_DOMAIN = 'dinaga-advice.firebaseapp.com';
+export const FIREBASE_DATABASE_URL = 'https://dinaga-advice.firebaseio.com';
+export const FIREBASE_PROJECT_ID = 'dinaga-advice';
+export const FIREBASE_STORAGE_BUCKET = 'dinaga-advice.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '400656871007';
+
+// export const FACEBOOK_APP_ID = <YOUR_FACEBOOK_APP_ID>;
